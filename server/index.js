2b91c4ddad8bdaf21d11d383ce94f1c48cd6de95
Tim La <tlamarketing111@gmail.com>
2022-03-14 15:59:44 -0400

    able to get users

--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const UserModel = require("./models/Users.js");
 
 mongoose.connect("mongodb+srv://tla2020:Learn2019@cluster0.fv0rv.mongodb.net/merntutorial?retryWrites=true&w=majority")
 
+app.get("/getUsers", (req, res) => {
+    UserModel.find({}, (err, result) => {
+        if (err) {
+            res.json(err)
+        } else {
+            res.json(result)
+        }
+    })
+});
 
 app.listen(3001, () => {
 
